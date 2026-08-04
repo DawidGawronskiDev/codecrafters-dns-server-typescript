2918edff8e963687fb6d4a690530de5fd15bb406
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2026-08-04 10:51:53 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -43,7 +43,7 @@ udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
       .withName(question.name)
       .withType(1)
       .withClass(1)
-      .withTimeToLive(300)
+      .withTimeToLive(120)
       .withDataLength(4)
       .build();
 
